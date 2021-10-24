3b725ad28e9ca697aa4f5b3f6a061d0f0c911a48
Diana Holoborodko <diana.holoborodko@gmail.com>
2021-10-24 19:30:33 +0300

    feat: add style burger-menu

---
+++ GoCorona/page/js/script.js
@@ -0,0 +1,11 @@
+
+const iconMenu = document.querySelector('.header__burger');
+const menuBody = document.querySelector('.menu-body');
+
+if (iconMenu) {
+	iconMenu.addEventListener('click', function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
+}
