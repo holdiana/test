f8e68a9f5553ea568113514cc0d2d8b347c833f1
Diana Holoborodko <diana.holoborodko@gmail.com>
2021-11-23 00:57:44 +0200

    fix: fix slider item, add slick-slider settings

--- georgian_restourant/src/js/script.js
+++ georgian_restourant/src/js/script.js
@@ -8,3 +8,42 @@ if (iconMenu) {
 		menuBody.classList.toggle('_active');
 	})
 }
+
+//slider
+$('.carousel__inner').slick({
+	prevArrow: '<button type="button"  class="carousel-control carousel-control__right"><img src="./icons/right-arrow.svg" alt="prev icon" class="carousel__arrow-icon"></button>',
+	nextArrow: '<button type="button" class="carousel-control carousel-control__left"><img src="./icons/left-arrow.svg" alt="next icon" class="carousel__arrow-icon"></button>',
+	centerMode: true,
+	centerPadding: '60px',
+	slidesToShow: 5,
+	dots: false,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 555,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '20px',
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			}
+		}
+	]
+});
